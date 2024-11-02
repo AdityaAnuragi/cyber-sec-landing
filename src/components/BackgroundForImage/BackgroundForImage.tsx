ed59d392e21fde879a888eaca6c1e6e098437a17
AdityaAnuragi <110617126+AdityaAnuragi@users.noreply.github.com>
2024-11-02 20:13:04 +0530

    hard-coded style

--- src/components/BackgroundForImage/BackgroundForImage.tsx
+++ src/components/BackgroundForImage/BackgroundForImage.tsx
@@ -6,9 +6,17 @@ type BackgroundForImageProps = {
   children: ReactNode
 }
 
-export function BackgroundForImage({children}: BackgroundForImageProps) {
+export function BackgroundForImage({ children }: BackgroundForImageProps) {
+
+  const styleForDiv = {
+    width: "18.75rem",
+    height: "6.25rem",
+    marginLeft: "auto",
+    marginRight: "auto"
+  }
+
   return (
-    <div className={`${BackgroundForImageStyle.background}`} >
+    <div className={`${BackgroundForImageStyle.background}`} style={styleForDiv} >
       {children}
     </div>
   )
